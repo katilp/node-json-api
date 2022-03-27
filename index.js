4b5cc3080c3447cb06f403acceaa3d9e59359ebe
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2022-03-27 16:05:34 +0200

    fix reply for number and add run ranges

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var fs=require('fs');
 var data=fs.readFileSync('cms_release_info.json');
 var years=JSON.parse(data);
+var data2=fs.readFileSync('run_ranges.json');
+var run_eras=JSON.parse(data2);
 const express = require("express");
 const url = require('url');
 var favicon = require('serve-favicon');
@@ -28,7 +30,7 @@ function welcome(request,response)
 {
 	console.log(request.get('Host'));
 	var reply={
-		message: "Add a path, available paths: /years (or /years/<YYYY> or /years/<YYYY>/<key>)"
+		message: "Add a path, available paths: /years and /runeras (or: /years/<YYYY>  /years/<YYYY>/<key>  /runeras/<RunYYYYN>  /runeras/<RunYYYYN>/<key>)"
 	}
     response.send(reply);
 }
@@ -39,25 +41,49 @@ function searchYear(request,response)
 {
 	var year=request.params.year;
 	var key=request.params.mykey;
+	var reply;
 
 	if(years[year])
 	{
 		if (key)
 		{
-			var reply=years[year][key];
+			reply=String(years[year][key]);
 		}
 		else
 		{
-			var reply=years[year];
+			reply=years[year];
 		}	
 	}
 	else
 	{
-		var reply=years;
+		reply=years;
 	}
     console.log(reply);
 	response.send(reply);
 }
 
+app.get('/runeras/:era?/:mykey?',searchEra);
+function searchEra(request,response)
+{
+	var era=request.params.era;
+	var key=request.params.mykey;
+	var reply;
+	
+	if (era)
+	{
+		const this_era = run_eras.filter(an_era => an_era.run_era == era);
+		reply=this_era[0];
+		if (key)
+		{
+			reply=String(this_era[0][key]);
+		}
+	}
+	else
+	{
+		reply=run_eras;
+	}
 
+    console.log(reply);
+	response.send(reply);
+}
 
